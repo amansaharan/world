1c08e1afc8bcb2f70f29478c151ef32a37098a44
AMAN SAHARAN <aman@AMANs-MacBook-Pro.local>
2018-12-20 19:13:40 +0530

    Updates

---
+++ precache-manifest.a46806daf871cbe2bde5232fa76e1097.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "619be0ee61be71489dfb",
+    "url": "/world/static/css/main.4d9d5d2c.chunk.css"
+  },
+  {
+    "revision": "619be0ee61be71489dfb",
+    "url": "/world/static/js/main.619be0ee.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/world/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "0ae1b2e3c8e1d4a2d624",
+    "url": "/world/static/js/runtime~main.0ae1b2e3.js"
+  },
+  {
+    "revision": "334ee0db912ab0b310269d83fb2cc55d",
+    "url": "/world/index.html"
+  }
+];
